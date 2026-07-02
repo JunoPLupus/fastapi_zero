dcd35e689ebedcdafe99535ba513d0531d290a60
Juno Lupus <junoplupus@gmail.com>
2026-07-02 14:43:40 -0300

    feat: Adiciona rotas CRUD no app.py

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from starlette.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND
+
+from fastapi_zero.models import Message, User, UserDB, UserList, UserPublic
 
 app = FastAPI()
+database = []
 
 
-@app.get('/')
+@app.get('/', status_code=HTTP_200_OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/users/', status_code=HTTP_200_OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.post('/users/', status_code=HTTP_201_CREATED, response_model=UserPublic)
+def create_user(user: User):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: User):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND, detail='User not found'
+        )
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
